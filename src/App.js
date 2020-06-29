ddbe78588dc5baacfb11e63bb75981939c4a14e6
will.crossland <will.crossland01@bbc.co.uk>
2020-06-29 11:16:43 +0100

    Make App a non-connected Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import './app.css'
 import FmSynth from './fm-synth/FmSynth'
 import DOMPush from './push/DOMPush'
-import { activeGridSelect, activePads, gridPadPressed, gridSelectPressed } from './push'
 
-const mapStateToProps = state => ({
-  activeGridSelect: activeGridSelect(state),
-  activePads: activePads(state)
-})
-
-const App = ({ activeGridSelect, activePads, gridPadPressed, gridSelectPressed }) => (
+const App = () => (
   <>
     <h1>Push FM</h1>
     <FmSynth />
@@ -18,4 +11,4 @@ const App = ({ activeGridSelect, activePads, gridPadPressed, gridSelectPressed }
   </>
 )
 
-export default connect(mapStateToProps, { gridPadPressed, gridSelectPressed })(App)
+export default App
